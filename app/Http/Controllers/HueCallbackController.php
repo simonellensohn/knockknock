89bf778419722c466b11ffc7119a49ff148cb84e
Simon Ellensohn <>
2021-12-13 21:54:44 +0100

    wip

--- app/Http/Controllers/HueCallbackController.php
+++ app/Http/Controllers/HueCallbackController.php
@@ -4,26 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Services\Hue\HueApi;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\JsonResponse;
 
 class HueCallbackController extends Controller
 {
-    public function __invoke(Request $request, HueApi $hueApi)
+    public function __invoke(Request $request, HueApi $hueApi): JsonResponse
     {
-        if (Storage::exists('hue.json')) {
-            abort(400, 'Config file already exists, aborting.');
-        }
+        $request->validate(['code' => 'required']);
 
-        if ($code = $request->input('code')) {
-            $hueApi->fetchAccessToken($code);
+        $hueApi->fetchAccessToken($request->input('code'));
 
-            return response()->json([
-                'data' => [
-                    'username' => $hueApi->createUser(),
-                ],
-            ]);
-        }
-
-        abort(400, 'Invalid request, code missing.');
+        return response()->json([
+            'data' => [
+                'username' => $hueApi->createUser(),
+            ],
+        ]);
     }
 }
