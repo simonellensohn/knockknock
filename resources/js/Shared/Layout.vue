97e144d8d092c6221495a5b3a8e62e2c2af3334a
Simon Ellensohn <>
2021-12-14 01:35:52 +0100

    wip

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -13,16 +13,16 @@
                 <svg class="w-6 h-6 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
               </template>
               <template #dropdown>
-                <div class="mt-2 px-8 py-4 bg-indigo-800 rounded shadow-lg">
+                <div class="px-8 py-4 mt-2 bg-indigo-800 rounded shadow-lg">
                   <main-menu />
                 </div>
               </template>
             </dropdown>
           </div>
-          <div class="md:text-md flex items-center justify-end p-4 w-full text-sm bg-white border-b md:px-12 md:py-0">
+          <div class="flex items-center justify-end w-full p-4 text-sm bg-white border-b md:text-md md:px-12 md:py-0">
             <dropdown class="mt-1" placement="bottom-end">
               <template #default>
-                <div class="group flex items-center cursor-pointer select-none">
+                <div class="flex items-center cursor-pointer select-none group">
                   <div class="mr-1 text-gray-700 group-hover:text-indigo-600 focus:text-indigo-600 whitespace-nowrap">
                     <span>{{ auth.user.first_name }}</span>
                     <span class="hidden md:inline">&nbsp;{{ auth.user.last_name }}</span>
@@ -31,17 +31,17 @@
                 </div>
               </template>
               <template #dropdown>
-                <div class="mt-2 py-2 text-sm bg-white rounded shadow-xl">
+                <div class="py-2 mt-2 text-sm bg-white rounded shadow-xl">
                   <!-- <Link class="block px-6 py-2 hover:text-white hover:bg-indigo-500" :href="`/users/${auth.user.id}/edit`">My Profile</Link> -->
                   <!-- <Link class="block px-6 py-2 hover:text-white hover:bg-indigo-500" href="/users">Manage Users</Link> -->
-                  <Link class="block px-6 py-2 w-full text-left hover:text-white hover:bg-indigo-500" :href="$route('logout')" method="delete" as="button">Logout</Link>
+                  <Link class="block w-full px-6 py-2 text-left hover:text-white hover:bg-indigo-500" :href="$route('logout')" method="delete" as="button">Logout</Link>
                 </div>
               </template>
             </dropdown>
           </div>
         </div>
         <div class="md:flex md:flex-grow md:overflow-hidden">
-          <main-menu class="hidden flex-shrink-0 p-12 w-56 bg-indigo-800 overflow-y-auto md:block" />
+          <main-menu class="flex-shrink-0 hidden w-56 p-12 overflow-y-auto bg-indigo-800 md:block" />
           <div class="px-4 py-8 md:flex-1 md:p-12 md:overflow-y-auto" scroll-region>
             <flash-messages />
             <slot />
--- resources/js/Shared/LoadingButton.vue
+++ resources/js/Shared/LoadingButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button :disabled="loading" class="flex items-center">
-    <div v-if="loading" class="btn-spinner mr-2" />
+    <div v-if="loading" class="mr-2 btn-spinner" />
     <slot />
   </button>
 </template>
