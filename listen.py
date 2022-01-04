90122f0f39439fe0a8d90b349c372402ae14a0a4
Simon Ellensohn <>
2022-01-04 22:48:32 +0100

    wip

--- listen.py
+++ listen.py
@@ -7,23 +7,35 @@ import sounddevice as sd
 
 bells = []
 volumeThresholds = {}
-duration = 2
-setsToCheckFor = 2
+duration = 1
+setsToCheckFor = 3
 events = []
 averages = []
 baseUrl = 'https://knockknock.test/api'
-accessToken = '1|2g0Hlkf6zjEQCJr5TkEb3VSnQ3HGzJKig9LB7We4'
+accessToken = '1|F1UAuiR5XDfsqKq0nV96n7EDh2I3vsUUVusMkWAW'
 requestHeaders = {'Authorization': 'Bearer ' + accessToken}
+minRange = None
+maxRange = None
 
 
 def fetch_bells():
     response = requests.get(baseUrl + '/bells', headers=requestHeaders)
+
     return response.json().get('data')
 
 
-def set_threshold_map():
+def set_volume_range():
+    global minRange
+    global maxRange
+
     for bell in bells:
-        volumeThresholds[float(bell.get('threshold')) / 10] = bell.get('id')
+        value = float(bell.get('threshold'))
+
+        if (minRange == None or value < minRange):
+            minRange = value
+
+        if (maxRange == None or value > maxRange):
+            maxRange = value
 
 
 def audio_callback(indata, frames, time, status):
@@ -39,6 +51,7 @@ def listen_for_doorbell():
 def average_of_last_events():
     return sum(events) / len(events)
 
+
 def post_ring(bellId, volume):
     requests.post(
         baseUrl + '/bells/' + str(bellId) + '/ring',
@@ -46,8 +59,22 @@ def post_ring(bellId, volume):
         headers=requestHeaders,
     )
 
+
+def set_is_fluctuating(volumes):
+    isFluctuating = False
+    previousValue = None
+
+    for volume in volumes:
+        isFluctuating = previousValue and abs(previousValue - volume) > 3
+        previousValue = volume
+
+    return isFluctuating
+
+
 bells = fetch_bells()
-set_threshold_map()
+bells.sort(key=lambda bell: float(bell.get('threshold')), reverse=True)
+set_volume_range()
+
 
 while True:
     listen_for_doorbell()
@@ -55,24 +82,16 @@ while True:
     if (len(averages) >= setsToCheckFor):
         averages.pop(0)
 
-    average = average_of_last_events()
-
-    # False positive, start from scratch
-    if (average > max(volumeThresholds.keys())):
-        averages = []
-        events = []
-        continue
-
-    averages.append(average)
+    volume = average_of_last_events()
+    averages.append(volume)
+    average = np.average(averages)
     events = []
-    average = np.sum(averages)
 
-    if (any(average >= volume for volume in volumeThresholds)):
-        matchingBells = filter(lambda bell: bell.treshhold >= average, bells)
-        bellId = volumeThresholds[min(volumeThresholds.keys())]
+    if (average >= minRange and not set_is_fluctuating(averages)):
+        bell = [bell for bell in bells if float(bell.get('threshold')) <= average][0]
 
-        post_ring(bellId, average)
+        post_ring(bell.get('id'), average)
+
+        averages = []
 
         time.sleep(5)
-
-        averages = []
