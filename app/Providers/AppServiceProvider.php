fd93b9c04bb0b5aff3961a26ccbf4f87a3be4baf
Simon Ellensohn <ellensohn.simon@gmail.com>
2022-03-06 01:07:37 +0100

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,11 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         Model::unguard();
+
+        if ($this->app->environment('local')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     public function boot(): void
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 import { createApp, h } from 'vue'
 import { InertiaProgress } from '@inertiajs/progress'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
@@ -6,14 +5,11 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3'
 InertiaProgress.init()
 
 createInertiaApp({
-  resolve: name => require(`./Pages/${name}`),
-  title: title => `${title} - KnockKnock`,
+  resolve: (name) => require(`./Pages/${name}`),
+  title: (title) => (title ? `${title} - KnockKnock` : 'KnockKnock'),
   setup({ el, App, props, plugin }) {
     const app = createApp({ render: () => h(App, props) })
 
-    app.config.globalProperties.$route = window.route
-
-    app.use(plugin)
-      .mount(el)
+    app.use(plugin).mount(el)
   },
 })
