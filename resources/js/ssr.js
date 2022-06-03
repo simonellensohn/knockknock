315816a5465a4b251f1d4586284fbf99532b0e79
Simon Ellensohn <simon.ellensohn@aboutyou.com>
2022-06-03 09:30:43 +0200

    wip

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -2,8 +2,6 @@ import { createSSRApp, h } from 'vue'
 import { renderToString } from '@vue/server-renderer'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import createServer from '@inertiajs/server'
-import { ZiggyVue } from 'ziggy'
-import route from 'ziggy'
 
 createServer((page) =>
   createInertiaApp({
@@ -12,13 +10,9 @@ createServer((page) =>
     resolve: (name) => require(`./Pages/${name}`),
     title: (title) => (title ? `${title} - KnockKnock` : 'KnockKnock'),
     setup({ app, props, plugin }) {
-      app.config.globalProperties.$route = route
-
       return createSSRApp({
         render: () => h(app, props),
-      })
-        .use(plugin)
-        .use(ZiggyVue, page.props.ziggy)
+      }).use(plugin)
     },
   }),
 )
